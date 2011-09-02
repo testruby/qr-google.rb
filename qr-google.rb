d678a48acc7d7ec52ffb4937c567f13c4dbb0f1c
TestRuby <testruby@rambler.ru>
2011-09-02 10:04:43 +0400

    Добавлено описание

--- qr-google.rb
+++ qr-google.rb
@@ -1,14 +1,16 @@
+#!/usr/bin/env ruby
 require 'rubygems'
 require 'open-uri'
 
 LINK = "http://chart.googleapis.com/chart"
-def qr(text="Hello World", width=150, height=150)
+def qr(text="Hello from ruby", width=150, height=150)
 	text = URI.encode(text)
 	code = open("#{LINK}?chs=#{width}x#{height}&cht=qr&chl=#{text}").read
 	return code
 end
+text = ARGV[0] ? ARGV[0] : "Hello from ruby"
 width = ARGV[1] ? ARGV[1] : "150"
 height = ARGV[2] ? ARGV[2] : "150"
-text = ARGV[0] ? ARGV[0] : "Hello from ruby"
+
 puts qr(text, width, height)
 
